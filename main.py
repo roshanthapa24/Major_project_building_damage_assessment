4927103ea50d34bd6585763c230e8c0589817ccb
Roshan Thapa <roshanthapaa24@gmail.com>
2026-04-18 12:49:28 +0545

    initial commit

---
+++ main.py
@@ -0,0 +1,45 @@
+from typing import Union
+
+from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import FileResponse
+import os
+from random import randint
+import uuid
+from aimodel_pre import detectImage as detectImagePre
+
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+IMAGEDIR='.'
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.post("/process_image_pre/")
+async def process_image(file: UploadFile = File(...)):
+    file.filename = f"{uuid.uuid4()}.png"
+    contents = await file.read()
+
+    # Save the file
+    with open(f"{IMAGEDIR}/{file.filename}", "wb") as f:
+        f.write(contents)
+
+    print('File received and saved:', file.filename)
+
+    detectImagePre(file.filename)
+
+    processed_image_filename='detection_pre.png'
+    
+    print('Image processed and saved:', processed_image_filename)
+
+    # Return the processed image
+    return FileResponse(f"{IMAGEDIR}/{processed_image_filename}", media_type="image/png", filename=processed_image_filename)
